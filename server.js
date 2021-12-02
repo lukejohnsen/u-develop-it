da08df2a573aa82a359afba9936041c2822def2d
lukejohnsen <johnsen.luke@gmail.com>
2021-12-02 16:23:28 -0700

    push after figuring out mysql issues with machine

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: 'root',
+        password: 'rootroot',
         database: 'election',
     },
     console.log('Connected to the election database.')
